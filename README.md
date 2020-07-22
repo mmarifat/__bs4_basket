33077738155fd62e2663d85d25d0c7e89336485d
mmarifat <mma.rifat66@gmail.com>
2020-07-22 16:35:45 +0600

    requirements + readme update

---
+++ README.md
@@ -0,0 +1,34 @@
+# BeautifulSoup Scraping
+
+## Introduction
+
+This python script will scrap thousands of data from basketball-reference.com about sports descriptions.
+
+## Install the dependencies
+```bash
+pip3 install -r requirements.txt
+
+# or (for python 2)
+
+pip install -r requirements.txt
+```
+
+### Start
+```bash
+python3 basketball_reference.py
+
+# or (for python2)
+
+python basketball_reference.py
+```
+
+### Notice
+
+Output.csv contains already collected data. Renaming it will affect
+
+### About basketball-reference.com
+
+Primary Aim is to answer their users' questions with the easiest-to-use, fastest, and most complete sports statistics anywhere.
+```bash
+See [basketball-reference.com](https://www.basketball-reference.com).
+```
---
+++ basketball_reference.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import string
+
+content = []
+try:
+    # page iteration
+    for ch in string.ascii_lowercase[:26]:
+        url = "https://www.basketball-reference.com/players/"
+        res = requests.get(url + ch)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        for data in soup.find_all('table', {'id' : 'players'}):
+            for eachData in data.find_all('tr'):
+                pName = eachData.find('th').text
+                cells =  eachData.find_all('td')
+                if not cells:
+                    continue
+                con = [x.text.strip() for x in cells]
+                con.insert(0, pName)
+                content.append(con)
+        print("Page", ch, "done.")
+
+    # Final content - a huge array
+    print(content)
+
+    with open("output.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Player', 'Form', 'To', 'Pos', 'Ht', 'Wt', 'Birth Date', 'College']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        csvfile.write('\ufeff')
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
+        writer.writerows(content)
+
+except Exception as e:
+    print(e)
+
+
